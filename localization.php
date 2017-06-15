216b24c1b2a444e9ea9839b279c71ce24d80292e
Ueli Bosshard <ubosshard@gmail.com>
2017-06-15 08:46:09 +0200

    bill generator adjusted and added french and italian translations

--- localization.php
+++ localization.php
@@ -1,33 +1,59 @@
 <?php
 
-$TEXT['QR-bill payment']    = "QR-bill payment";
-$TEXT['Supports']           = "Supports";
-$TEXT['Credittransfer']     = "Credittransfer";
-$TEXT['Currency']           = "Currency";
-$TEXT['Amount']             = "Amount";
-$TEXT['Account']            = "Account";
-$TEXT['Creditor']           = "Creditor";
-$TEXT['Ultimate creditor']  = "Ultimate creditor";
-$TEXT['Reference Number']   = "Reference Number";
-$TEXT['Debtor']             = "Debtor";
-$TEXT['Due date']           = "Due date";
+$TEXT['QR-bill payment']        = "QR-bill payment";
+$TEXT['Supports']               = "Supports";
+$TEXT['Credit transfer']        = "Credit transfer";
+$TEXT['Account']                = "Account";
+$TEXT['Creditor']               = "Creditor";
+$TEXT['Ultimate creditor']      = "Ultimate creditor";
+$TEXT['Reference Number']       = "Reference Number";
+$TEXT['Additional information'] = "Additional information";
+$TEXT['Debtor']                 = "Debtor";
+$TEXT['Due date']               = "Due date";
+$TEXT['Currency']               = "Currency";
+$TEXT['Amount']                 = "Amount";
 
 if ($_POST['language'] == "DE") {
-  $TEXT['QR-bill payment']    = "Zahlteil QR-Rechnung";
-  $TEXT['Supports']           = "Unterstützt";
-  $TEXT['Credittransfer']     = "Ueberweisung";
-  $TEXT['Currency']           = "Währung";
-  $TEXT['Amount']             = "Betrag";
-  $TEXT['Account']            = "Konto";
-  $TEXT['Creditor']           = "Zahlungsempfänger";
-  $TEXT['Ultimate creditor']  = "Endgültiger Zahlungsempfänger";
-  $TEXT['Reference Number']   = "Referenznummer";
-  $TEXT['Debtor']             = "Zahlungspflichtiger";
-  $TEXT['Due date']           = "Zahlbar bis";
+  $TEXT['QR-bill payment']        = "Zahlteil QR-Rechnung";
+  $TEXT['Supports']               = "Unterstützt";
+  $TEXT['Credit transfer']        = "Ueberweisung";
+  $TEXT['Account']                = "Konto";
+  $TEXT['Creditor']               = "Zahlungsempfänger";
+  $TEXT['Ultimate creditor']      = "Endgültiger Zahlungsempfänger";
+  $TEXT['Reference Number']       = "Referenznummer";
+  $TEXT['Additional information'] = "Zusätzliche Informationen";
+  $TEXT['Debtor']                 = "Zahlungspflichtiger";
+  $TEXT['Due date']               = "Zahlbar bis";
+  $TEXT['Currency']               = "Währung";
+  $TEXT['Amount']                 = "Betrag";
 
 } else if ($_POST['language'] == "FR") {
+  $TEXT['QR-bill payment']        = "Section paiement QR-facture";
+  $TEXT['Supports']               = "Support";
+  $TEXT['Credit transfer']        = "Virement";
+  $TEXT['Account']                = "Compte";
+  $TEXT['Creditor']               = "Bénéficiaire";
+  $TEXT['Ultimate creditor']      = "Bénéficiaire final";
+  $TEXT['Reference Number']       = "Numéro de référenc";
+  $TEXT['Additional information'] = "Informations supplémentaires";
+  $TEXT['Debtor']                 = "Débiteur";
+  $TEXT['Due date']               = "A payer jusqu'au";
+  $TEXT['Currency']               = "Monnaie";
+  $TEXT['Amount']                 = "Montant";
 
 } else if ($_POST['language'] == "IT") {
+  $TEXT['QR-bill payment']        = "Sezione pagamento QR-fattura";
+  $TEXT['Supports']               = "Sostiene";
+  $TEXT['Credit transfer']        = "Bonifico";
+  $TEXT['Account']                = "Conto";
+  $TEXT['Creditor']               = "Beneficiario";
+  $TEXT['Ultimate creditor']      = "Beneficiario finale";
+  $TEXT['Reference Number']       = "Numero di riferimento";
+  $TEXT['Additional information'] = "Informazioni supplementar";
+  $TEXT['Debtor']                 = "Debitore";
+  $TEXT['Due date']               = "Da pagare entro il";
+  $TEXT['Currency']               = "Valuta";
+  $TEXT['Amount']                 = "Importo";
 
 }
 
--- qrbillgen.php
+++ qrbillgen.php
@@ -32,22 +32,34 @@ if (isset($_REQUEST['QRCH_CdtrInf_IBAN'])) {
   $data .= $_REQUEST['QRCH_UltmtDbtr_Ctry'] . $delim;
   $data .= $_REQUEST['QRCH_RmtInf_Tp'] . $delim;
   $data .= $_REQUEST['QRCH_RmtInf_Ref'] . $delim;
-  $data .= $_REQUEST['QRCH_RmtInf_Ustrd'] . $delim;
-  $data .= $_REQUEST['QRCH_AltPmtInf_AltPmt'] . $delim;
-  $data .= $_REQUEST['QRCH_AltPmtInf_AltPmt2'];
+  $data .= $_REQUEST['QRCH_RmtInf_Ustrd'];
   
+  if (strlen($_REQUEST['QRCH_AltPmtInf_AltPmt']) > 0)
+    $data .= $delim . $_REQUEST['QRCH_AltPmtInf_AltPmt'] ;
+
+  if (strlen($_REQUEST['QRCH_AltPmtInf_AltPmt2']) > 0)
+    $data .= $delim . $_REQUEST['QRCH_AltPmtInf_AltPmt2'];
   
-  $creditor = $_REQUEST['QRCH_CdtrInf_Cdtr_Name'] . "<br />" .
-              $_REQUEST['QRCH_CdtrInf_Cdtr_StrtNm'] . " " . $_REQUEST['QRCH_CdtrInf_Cdtr_BldgNb'] . "<br />" .
-              $_REQUEST['QRCH_CdtrInf_Cdtr_Ctry'] . "-" . $_REQUEST['QRCH_CdtrInf_Cdtr_PstCd'] . " " . $_REQUEST['QRCH_CdtrInf_Cdtr_TwnNm'];
+
+  $creditor  = $_REQUEST['QRCH_CdtrInf_Cdtr_Name'];
+  if (strlen($_REQUEST['QRCH_CdtrInf_Cdtr_StrtNm']) > 0 || strlen($_REQUEST['QRCH_CdtrInf_Cdtr_BldgNb']) > 0)
+    $creditor .= "<br />" . $_REQUEST['QRCH_CdtrInf_Cdtr_StrtNm'] . " " . $_REQUEST['QRCH_CdtrInf_Cdtr_BldgNb'];
+  $creditor .= "<br />" . $_REQUEST['QRCH_CdtrInf_Cdtr_Ctry'] . "-" . $_REQUEST['QRCH_CdtrInf_Cdtr_PstCd'] . " " . $_REQUEST['QRCH_CdtrInf_Cdtr_TwnNm'];
   
-  $ultimate_creditor = $_REQUEST['QRCH_UltmtCdtr_Name'] . "<br />" .
-              $_REQUEST['QRCH_UltmtCdtr_StrtNm'] . " " . $_REQUEST['QRCH_UltmtCdtr_BldgNb'] . "<br />" .
-              $_REQUEST['QRCH_UltmtCdtr_Ctry'] . "-" . $_REQUEST['QRCH_UltmtCdtr_PstCd'] . " " . $_REQUEST['QRCH_UltmtCdtr_TwnNm'];
+
+  if (strlen($_REQUEST['QRCH_UltmtCdtr_Name']) > 0) {
+    $ultimate_creditor = $_REQUEST['QRCH_UltmtCdtr_Name'];
+    if (strlen($_REQUEST['QRCH_UltmtCdtr_StrtNm']) > 0 || strlen($_REQUEST['QRCH_UltmtCdtr_BldgNb']) > 0)
+      $ultimate_creditor .= "<br />" . $_REQUEST['QRCH_UltmtCdtr_StrtNm'] . " " . $_REQUEST['QRCH_UltmtCdtr_BldgNb'];
+    $ultimate_creditor .= "<br />" . $_REQUEST['QRCH_UltmtCdtr_Ctry'] . "-" . $_REQUEST['QRCH_UltmtCdtr_PstCd'] . " " . $_REQUEST['QRCH_UltmtCdtr_TwnNm'];
+  } else { $ultimate_creditor = ""; }
   
-  $debtor =   $_REQUEST['QRCH_UltmtDbtr_Name'] . "<br />" .
-              $_REQUEST['QRCH_UltmtDbtr_StrtNm'] . " " . $_REQUEST['QRCH_UltmtDbtr_BldgNb'] . "<br />" .
-              $_REQUEST['QRCH_UltmtDbtr_Ctry'] . "-" . $_REQUEST['QRCH_UltmtDbtr_PstCd'] . " " . $_REQUEST['QRCH_UltmtDbtr_TwnNm'];
+  if (strlen($_REQUEST['QRCH_UltmtDbtr_Name']) > 0) {
+    $debtor = $_REQUEST['QRCH_UltmtDbtr_Name'];
+    if (strlen($_REQUEST['QRCH_UltmtDbtr_StrtNm']) > 0 || strlen($_REQUEST['QRCH_UltmtDbtr_BldgNb']) > 0)
+      $debtor .= "<br />" . $_REQUEST['QRCH_UltmtDbtr_StrtNm'] . " " . $_REQUEST['QRCH_UltmtDbtr_BldgNb'];
+    $debtor .= "<br />" . $_REQUEST['QRCH_UltmtDbtr_Ctry'] . "-" . $_REQUEST['QRCH_UltmtDbtr_PstCd'] . " " . $_REQUEST['QRCH_UltmtDbtr_TwnNm'];
+  }
   
   include('phpqrcode.php');
   
@@ -67,7 +79,7 @@ if (isset($_REQUEST['QRCH_CdtrInf_IBAN'])) {
       <div id="title"><?php echo $TEXT['QR-bill payment']; ?></div>
       <div id="scheme">
          <div id="texttitle"><?php echo $TEXT['Supports']; ?></div>
-         <div id="content"><?php echo $TEXT['Credittransfer']; ?></div>
+         <div id="content"><?php echo $TEXT['Credit transfer']; ?></div>
        </div>
       <div id="qrcode"><?php echo $svgCode; ?></div>
       <div id="amount">
@@ -77,7 +89,35 @@ if (isset($_REQUEST['QRCH_CdtrInf_IBAN'])) {
         </div>
         <div id="right">
           <div id="texttitle"><?php echo $TEXT['Amount']; ?></div>
-          <div id="content"><?php echo $_REQUEST['QRCH_CcyAmtDate_Amt']; ?></div>
+          <div id="content">
+          <?php
+            if ($_REQUEST['QRCH_CcyAmtDate_Amt'] != "")
+              echo $_REQUEST['QRCH_CcyAmtDate_Amt'];
+            else {
+          ?>
+
+<svg version="1.1" id="Ebene_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+   viewBox="0 0 113.4 42.5" width="100" style="enable-background:new 0 0 113.4 42.5;" xml:space="preserve">
+<g>
+  <rect width="8.5" height="0.8"/>
+  <rect x="-3.9" y="3.9" transform="matrix(-1.836970e-16 1 -1 -1.836970e-16 4.627 3.877)" width="8.5" height="0.8"/>
+</g>
+<g>
+  <rect x="-3.9" y="37.9" transform="matrix(6.123234e-17 -1 1 6.123234e-17 -37.893 38.643)" width="8.5" height="0.8"/>
+  <rect y="41.8" width="8.5" height="0.8"/>
+</g>
+<g>
+  <rect x="104.9" y="41.8" transform="matrix(-1 -1.224647e-16 1.224647e-16 -1 218.2761 84.29)" width="8.5" height="0.8"/>
+  <rect x="108.8" y="37.9" transform="matrix(6.123234e-17 -1 1 6.123234e-17 74.747 151.283)" width="8.5" height="0.8"/>
+</g>
+<g>
+  <rect x="108.8" y="3.9" transform="matrix(-1.836970e-16 1 -1 -1.836970e-16 117.267 -108.763)" width="8.5" height="0.8"/>
+  <rect x="104.9" transform="matrix(-1 -1.224647e-16 1.224647e-16 -1 218.2761 0.75)" width="8.5" height="0.8"/>
+</g>
+</svg>
+<?php } ?>
+
+          </div>
         </div>
         <div id="clear"></div>
       </div>
@@ -88,14 +128,51 @@ if (isset($_REQUEST['QRCH_CdtrInf_IBAN'])) {
         <div id="content"><?php echo $_REQUEST['QRCH_CdtrInf_IBAN']; ?></div>
         <div id="texttitle"><?php echo $TEXT['Creditor']; ?></div>
         <div id="content"><?php echo $creditor; ?></div>
+        <?php if($ultimate_creditor != "") { ?>
         <div id="texttitle"><?php echo $TEXT['Ultimate creditor']; ?></div>
         <div id="content"><?php echo $ultimate_creditor; ?></div>
+        <?php } ?>
+        <?php if($_REQUEST['QRCH_RmtInf_Ref'] != "") { ?>
         <div id="texttitle"><?php echo $TEXT['Reference Number']; ?></div>
         <div id="content"><?php echo $_REQUEST['QRCH_RmtInf_Ref']; ?></div>
+        <?php } ?>
+        <?php if($_REQUEST['QRCH_RmtInf_Ustrd'] != "") { ?>
+        <div id="texttitle"><?php echo $TEXT['Additional information']; ?></div>
+        <div id="content"><?php echo $_REQUEST['QRCH_RmtInf_Ustrd']; ?></div>
+        <?php } ?>
         <div id="texttitle"><?php echo $TEXT['Debtor']; ?></div>
-        <div id="content"><?php echo $debtor; ?></div>
+        <div id="content"><?php
+          if($debtor != "") {
+            echo $debtor;
+          } else {
+          ?>
+
+<svg version="1.1" id="Ebene_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+   viewBox="0 0 184.3 70.9" width="200" style="enable-background:new 0 0 184.3 70.9;" xml:space="preserve">
+<g>
+  <rect width="8.5" height="0.8"/>
+  <rect x="-3.9" y="3.9" transform="matrix(-1.836970e-16 1 -1 -1.836970e-16 4.627 3.877)" width="8.5" height="0.8"/>
+</g>
+<g>
+  <rect x="-3.9" y="66.2" transform="matrix(6.123234e-17 -1 1 6.123234e-17 -66.2392 66.9892)" width="8.5" height="0.8"/>
+  <rect y="70.1" width="8.5" height="0.8"/>
+</g>
+<g>
+  <rect x="175.7" y="70.1" transform="matrix(-1 -1.224647e-16 1.224647e-16 -1 360 140.9823)" width="8.5" height="0.8"/>
+  <rect x="179.6" y="66.2" transform="matrix(6.123234e-17 -1 1 6.123234e-17 117.2628 250.4911)" width="8.5" height="0.8"/>
+</g>
+<g>
+  <rect x="179.6" y="3.9" transform="matrix(-1.836970e-16 1 -1 -1.836970e-16 188.1289 -179.625)" width="8.5" height="0.8"/>
+  <rect x="175.7" transform="matrix(-1 -1.224647e-16 1.224647e-16 -1 360 0.75)" width="8.5" height="0.8"/>
+</g>
+</svg>
+
+        <?php } ?>
+        </div>
+        <?php if($_REQUEST['QRCH_CcyAmtDate_ReqdExctnDt'] != "") { ?>
         <div id="texttitle"><?php echo $TEXT['Due date']; ?></div>
         <div id="content"><?php echo $_REQUEST['QRCH_CcyAmtDate_ReqdExctnDt']; ?></div>
+        <?php } ?>
       </div>
     </div>
     <div id="clear"></div>
